166d3caedfb3dd9e164d42c2fce811023b0abf99
Vaneda <vaneda.phimmachack@gmail.com>
2019-04-23 20:08:17 -0700

    learning boolean & isNaN in boolean.js

---
+++ js/boolean.js
@@ -0,0 +1,13 @@
+// boolean data type
+var a = true; // 1
+var b = false; // 0
+
+console.log(12 == '12');
+console.log(24 === '24');
+
+console.log(isNaN(2000));
+console.log(isNaN('abc'));
+console.log(isNaN('2000'));
+console.log(isNaN(true));
+console.log(isNaN('04/23/2019'));
+

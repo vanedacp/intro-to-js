ab68d2063b6057cc23c48848e750a52d0ee0ceb7
Vaneda <vaneda.phimmachack@gmail.com>
2019-04-23 20:56:49 -0700

    learning object data type in object.js

---
+++ js/object.js
@@ -0,0 +1,31 @@
+// object data type
+var obj = {}; // empty object
+var car = {
+    make: 'honda',
+    model: 'accord',
+    year: 2012
+};
+console.log(car);
+console.log(car.model);
+console.log(car.year);
+console.log(car.make);
+
+
+var date = {};
+// add date, month, year
+var date = {
+    date: 23,
+    month: 'April',
+    year: 2019,
+    day: 'Tuesday'
+};
+console.log(date);
+// display Tuesday on console
+console.log(date.day);
+
+
+var userCred = {
+    username: 'vphim',
+    password: 'password'
+};
+console.log(userCred);

fd07af2d99f4bcce6a1d83aaae44715318448de5
Vaneda <vaneda.phimmachack@gmail.com>
2019-04-23 19:04:25 -0700

    learning about number data type in number.js

---
+++ js/number.js
@@ -0,0 +1,38 @@
+// Number data type
+var a = 10;
+console.log(a);
+var b = 100;
+console.log(b);
+var a = 20;
+console.log(a);
+var c = 0x1E; // hex value of 30
+console.log(c);
+var d = 0.5;
+console.log(d);
+
+// addition
+var sum = a + b;
+console.log(sum);
+
+// substraction
+var diff = d - c;
+console.log(diff);
+
+// multiplication
+var mult = a * d;
+console.log(mult);
+
+// division
+var div = a / d;
+console.log(div);
+var div = b / a;
+console.log(div);
+
+// modulus
+var mod = b % a;
+console.log(mod);
+var mod = b % c;
+console.log(mod);
+
+var e = '5'; // not a number; bbut is a string
+console.log(typeof e); // returns the type of variable e
